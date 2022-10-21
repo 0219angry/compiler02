a97fc5815943b101c94e6da1d45acae30eb46555
0219Angry <angry.yuma.0219@gmail.com>
2022-10-21 21:30:42 +0900

    scan.c update(init_scan)

--- scan.c
+++ scan.c
@@ -13,6 +13,17 @@ int init_scan(char *filename){
 }
 
 int scan(void){
+  char buf;
+  while(1){
+    if((buf = getc(fp)) == EOF){
+      return -1;
+    }
+    
+    
+  }
+}
+
+int isseparator(char buf){
 
 }
 
