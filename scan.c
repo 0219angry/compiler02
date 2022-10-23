cc74dd14a4ccc832f86afaf98309b4140f2ba528
0219Angry <angry.yuma.0219@gmail.com>
2022-10-23 20:34:28 +0900

    develop scan.c

--- scan.c
+++ scan.c
@@ -1,7 +1,13 @@
 #include "token-list.h"
 
+int isSeparator(void);
+int isSymbol(void);
+int getNewchar(void);
+
 int num_attr;
 char string_attr[MAXSTRSIZE];
+
+int cbuf[2];
 
 FILE * fp;
 
@@ -13,18 +19,70 @@ int init_scan(char *filename){
 }
 
 int scan(void){
-  char buf;
-  while(1){
-    if((buf = getc(fp)) == EOF){
-      return -1;
-    }
-    
-    
+  getNewchar();
+  if(getNewchar() == -1){
+    return -1;
+  }
+  
+}
+
+//for scan() functions
+
+int isSeparator(void){
+
+}
+
+int isSymbol(void){
+  switch(cbuf[0]){
+    case '+': return TPLUS;
+    case '-': return TMINUS;
+    case '*': return TSTAR;
+    case '=': return TEQUAL;
+    case '<': if(getNewchar() != 0){
+        if(cbuf[1] == '>'){
+          getNewchar();
+          return TNOTEQ;
+        }else if(cbuf[1] == '='){
+          getNewchar();
+          return TLEEQ;
+        }else{
+          return TLE;
+        }
+      }
+    case '>': if(getNewchar() != 0){
+        if(cbuf[1] == '='){
+          getNewchar();
+          return TGREQ;
+        }else{
+          return TGR;
+        }
+      }
+    case '(': return TLPAREN;
+    case ')': return TRPAREN;
+    case '[': return TLSQPAREN;
+    case ']': return TRSQPAREN;
+    case '.': return TDOT;
+    case ',': return TCOMMA;
+    case ':': if(getNewchar() != 0){
+        if(cbuf[1] == '='){
+          getNewchar();
+          return TASSIGN;
+        }else{
+          return TCOLON;
+        }
+      }
+    case ';': return TSEMI;
+    default : return -1;
   }
 }
 
-int isseparator(char buf){
-
+int getNewchar(void){
+  cbuf[0] = cbuf[1];
+  if(cbuf[0] == EOF){
+    return -1;
+  }
+  cbuf[1] = fgetc(fp);
+  return 0;
 }
 
 int get_linenum(void){
